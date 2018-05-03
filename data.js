dab38e52fabae878cd96bcd5eb02a49a57e7a00a
Marck Manga <Marck@thinkful.com>
2018-05-03 14:46:23 -0700

    Submit-Listener added. TasteDive Call Function added

---
+++ data.js
@@ -0,0 +1,8 @@
+const placeHolder = [
+	'Game Of Thrones',
+	'Post Malone',
+	'Kurt Vonnegut',
+	'Overwatch',
+	'Stranger Things',
+	'Marvel Comics',
+	'Web Development'];
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+//Placeholder Function
 $(document).ready(function(){
-        var placeHolder = ['one','two','three','four','five'];
         var n=0;
         var loopLength=placeHolder.length;
 
@@ -14,3 +14,85 @@ $(document).ready(function(){
            }
         },2000);
     });
+
+////////////////////////////////////////TasteKid API//////////////////////////////////////////////////
+const TasteDiveSearchUrl = "https://tastedive.com/api/similar"
+const TasteDiveAPIKey = "307161-APICapst-JBLGJD7N"
+
+//getDatafromAPIFunction
+function getDatafromTasteDiveAPI(searchTerm, callback) {
+  const settings = {
+    url: TasteDiveSearchUrl,
+    data: {
+      k: TasteDiveAPIKey,
+      q: `${searchTerm}`,
+      info: 1,
+      limit: 5
+    },
+    dataType: 'jsonp',
+    type: 'GET',
+    success: callback
+  };
+  $.getJSON(TasteDiveSearchUrl, settings, callback);
+  console.log("getDatafromTasteDiveAPI works");
+}
+
+//renderResults
+/*function renderTasteDiveResults () {
+
+}
+
+function displayTasteDiveResults(data) {
+  let results = data.items.map((item, index) => renderResults(item));
+  $('.search-results').html(results);
+}*/
+
+///////////////////////////////////////////Meeetup API//////////////////////////////////////////////
+/*const meetupSearchUrl = "http://api.meetup.com"
+const meetupAPIKey ="43452e79373956592e2a1939544e1d23"
+
+//getDatafromAPIFunction
+function getDatafromMeetupAPI(searchTerm, callback) {
+  const settings = {
+    url: meetupSearchUrl,
+    data: {
+      key: meetupAPIKey,
+      q: `${searchTerm}`
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  $.getJSON(meetupSearchUrl, settings, callback);
+  console.log("Meetup works?");
+}
+
+
+
+//renderResults
+//function renderResults(result)
+
+
+
+
+
+//displayMeetupResults
+function displayMeetupResults(data) {
+  let results = data.items.map((item, index) => renderResults(item));
+  $('.search-results').html(results);
+}*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//Event Listener 
+function listenSubmit () {
+  $('#search-form').submit(event => {
+    event.preventDefault();
+    let searchTerm = document.getElementById('js-input').value;
+    console.log(searchTerm);
+    getDatafromTasteDiveAPI(searchTerm, /*displayTasteDiveResults*/);
+    //getDatafromMeetupAPI(searchTerm, displayMeetupResults);
+  })
+}
+
+listenSubmit();
